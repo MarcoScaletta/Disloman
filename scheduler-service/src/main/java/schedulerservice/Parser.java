fbecec585e8caed3e5fd0f4052bd4005f441b4c0
Marco Scaletta <scalettamarco@yahoo.it>
2019-05-11 18:43:49 +0200

    Created parser

---
+++ scheduler-service/src/main/java/schedulerservice/Parser.java
@@ -0,0 +1,32 @@
+package schedulerservice;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private final HashMap<String, String> map;
+
+    public Parser(String [] dataIDs,String[]regexes) throws Exception {
+        if(dataIDs.length != regexes.length)
+            throw new Exception("Data Ids and Regexes must have same lenght");
+        map = new HashMap<>();
+        for (int i = 0; i < dataIDs.length; i++) {
+            map.put(dataIDs[i],regexes[i]);
+        }
+    }
+
+    public HashMap<String, String> getMap() {
+        return map;
+    }
+
+    public String findVal(String dataID, String toFind){
+        Matcher matcher = Pattern.compile(map.get(dataID)).matcher(toFind);
+        if(matcher.find())
+            return matcher.group(1);
+        else
+            return null;
+
+    }
+}
