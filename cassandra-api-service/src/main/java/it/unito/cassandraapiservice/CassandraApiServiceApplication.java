1404a413d202557cd7c9b8e2ba908c2c142fe191
Marco Scaletta <scalettamarco@yahoo.it>
2019-05-15 19:24:26 +0200

    Removed Old Repository (HAL is useless and problematic) + cleaned code

--- cassandra-api-service/src/main/java/it/unito/cassandraapiservice/CassandraApiServiceApplication.java
+++ cassandra-api-service/src/main/java/it/unito/cassandraapiservice/CassandraApiServiceApplication.java
@@ -1,6 +1,5 @@
 package it.unito.cassandraapiservice;
 
-//import it.unito.oeecomputationservice.events.GreetingStreams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
