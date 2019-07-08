5f9feb5e827dbbadf6818667cd2205fe3eaf254e
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-08 03:00:06 +0200

    Add bean for persistance operations

--- cassandra-api-service/src/main/java/it/unito/cassandraapiservice/CassandraApiServiceApplication.java
+++ cassandra-api-service/src/main/java/it/unito/cassandraapiservice/CassandraApiServiceApplication.java
@@ -1,12 +1,42 @@
 package it.unito.cassandraapiservice;
 
+import it.unito.cassandraapiservice.controller.GenericMachineOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.BilanciaOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.EtichettatriceOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.IncartonatriceOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.TappatriceOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
-//@EnableBinding(GreetingStreams.class)
 @SpringBootApplication
-public class CassandraApiServiceApplication
-{
+public class CassandraApiServiceApplication{
+
+	@Bean
+	@Scope(value = "singleton")
+	public TappatriceOperations tappatriceOperations(){
+		return new TappatriceOperations();
+	}
+
+	@Bean
+	@Scope(value = "singleton")
+	public IncartonatriceOperations incartonatriceOperations(){
+		return new IncartonatriceOperations();
+	}
+
+	@Bean
+	@Scope(value = "singleton")
+	public EtichettatriceOperations etichettatriceOperations(){
+		return new EtichettatriceOperations();
+	}
+
+	@Bean
+	@Scope(value = "singleton")
+	public BilanciaOperations  bilanciaOperations(){
+		return new BilanciaOperations();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CassandraApiServiceApplication.class, args);
 	}
