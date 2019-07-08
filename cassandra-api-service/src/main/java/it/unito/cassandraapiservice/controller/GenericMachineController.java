4d1892a9b06b711fb0bca01d6856a97bed80a9f5
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-08 02:53:38 +0200

    Add unique controller

---
+++ cassandra-api-service/src/main/java/it/unito/cassandraapiservice/controller/GenericMachineController.java
@@ -0,0 +1,110 @@
+package it.unito.cassandraapiservice.controller;
+
+
+import it.unito.cassandraapiservice.model.apiobjects.*;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.BilanciaOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.EtichettatriceOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.IncartonatriceOperations;
+import it.unito.cassandraapiservice.model.machinepersistentoperations.TappatriceOperations;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class GenericMachineController {
+
+    @Autowired
+    TappatriceOperations tappatriceOperations;
+
+    @Autowired
+    IncartonatriceOperations incartonatriceOperations;
+
+    @Autowired
+    EtichettatriceOperations etichettatriceOperations;
+
+    @Autowired
+    BilanciaOperations bilanciaOperations;
+
+
+    @GetMapping("/api/ssb/")
+    public String test()
+    {
+        return "OK";
+    }
+
+    @GetMapping("/records/{macchina}/")
+    public RecordsList getRecords(@PathVariable String macchina){
+        return getMachineOperations(macchina).getRecords();
+    }
+
+    @PostMapping("/records/{macchina}/")
+    public RecordsList addRecords(@PathVariable String macchina,
+            @RequestBody RecordsList records){
+        return getMachineOperations(macchina).addRecords(records);
+    }
+
+
+    @DeleteMapping(value = "/records/{macchina}/{id}/",
+            produces = "application/json; charset=utf-8")
+    public String deleteRecord(@PathVariable String macchina, @PathVariable String id) {
+        return getMachineOperations(macchina).deleteRecord(id);
+    }
+
+
+    // EXTERNAL API
+
+    @GetMapping("api/ssb/risultati/{macchina}/")
+    public RisultatiList getRisultati(@PathVariable String macchina){
+        GenericMachineOperations machineOperations = getMachineOperations(macchina);
+        if (machineOperations == null)
+            return null;
+        return machineOperations.getRisultati();
+    }
+
+    @GetMapping("api/ssb/risultati_commessa/{macchina}/commessa/{commessa}/")
+    public RisultatiCommessaList getRisultatiCommessa(
+            @PathVariable String macchina,
+            @PathVariable String commessa){
+        GenericMachineOperations machineOperations = getMachineOperations(macchina);
+        if (machineOperations == null)
+            return null;
+        return machineOperations.getRisultatiCommessa(commessa);
+    }
+
+    @GetMapping("api/ssb/risultati_commessa_turno/{macchina}/{commessa}/{turno}/")
+    public RisultatiCommessaTurnoList getRisultatiCommessaTurno(
+            @PathVariable String macchina,
+            @PathVariable String commessa,
+            @PathVariable String turno){
+        GenericMachineOperations machineOperations = getMachineOperations(macchina);
+        if (machineOperations == null)
+            return null;
+        return machineOperations.getRisultatiCommessaTurno(commessa, turno);
+    }
+
+    @GetMapping("api/ssb/risultati_odl_turno/{macchina}/{odl}/{turno}/")
+    public RisultatiODLTurnoList getRisultatiODLTurno(
+            @PathVariable String macchina,
+            @PathVariable String odl,
+            @PathVariable String turno){
+        GenericMachineOperations machineOperations = getMachineOperations(macchina);
+        if (machineOperations == null)
+            return null;
+        return machineOperations.getRisultatiODLTurno(odl, turno);
+    }
+
+
+    public GenericMachineOperations getMachineOperations(String nomeMacchina){
+
+        switch (nomeMacchina){
+            case "bilancia":return bilanciaOperations;
+            case "etichettatrice": return etichettatriceOperations;
+            case "tappatrice": return tappatriceOperations;
+            case "incartonatrice": return incartonatriceOperations;
+            default: return null;
+        }
+
+    }
+
+}
