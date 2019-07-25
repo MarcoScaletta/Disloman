c8fdadc0239e39eb34bf5f19a465958247949bec
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-25 16:22:50 +0200

    Delete Fase and ODL objects

--- cassandra-api-service/src/main/java/it/unito/cassandraapiservice/model/repository/FaseRepository.java
+++
@@ -1,7 +0,0 @@
-package it.unito.cassandraapiservice.model.repository;
-
-import it.unito.cassandraapiservice.model.impl.smartshareobjects.Fase;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FaseRepository extends CrudRepository<Fase, String> {
-}
--- cassandra-api-service/src/main/java/it/unito/cassandraapiservice/model/repository/ODLRepository.java
+++
@@ -1,8 +0,0 @@
-package it.unito.cassandraapiservice.model.repository;
-
-import it.unito.cassandraapiservice.model.impl.smartshareobjects.ODL;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ODLRepository extends CrudRepository<ODL, String> {
-
-}
