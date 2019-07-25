a84e955e9b81f64ba5deced54c35dc18e7a9f3d1
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-25 17:00:39 +0200

    Clean code

--- smarthinge-api-service/src/main/java/smarthingeapiservice/SmartHingeAPIServiceApplication.java
+++ smarthinge-api-service/src/main/java/smarthingeapiservice/SmartHingeAPIServiceApplication.java
@@ -3,13 +3,10 @@ package smarthingeapiservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @EnableScheduling
 @SpringBootApplication
@@ -53,20 +50,6 @@ public class SmartHingeAPIServiceApplication {
     public QueryMachine queryEtichettatrice(){
         return new QueryMachine(etichettatriceCode, etichettatriceOutputCode);
     }
-
-
-    @Bean
-    @Scope(value = "singleton")
-    public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSourceBuilder.url(
-                "jdbc:sqlserver://;serverName=192.168.120.30;port=1433;databaseName=ORC_roh_ODM");
-        dataSourceBuilder.username("Orchestra");
-        dataSourceBuilder.password("Password2019");
-        return dataSourceBuilder.build();
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(SmartHingeAPIServiceApplication.class, args);
