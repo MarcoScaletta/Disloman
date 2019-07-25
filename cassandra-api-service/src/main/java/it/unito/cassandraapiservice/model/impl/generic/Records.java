904e21528d5f6a16e84259a59fb1e56945870299
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-25 16:12:53 +0200

    Add constructor for easier creation of Records objects

--- cassandra-api-service/src/main/java/it/unito/cassandraapiservice/model/impl/generic/Records.java
+++ cassandra-api-service/src/main/java/it/unito/cassandraapiservice/model/impl/generic/Records.java
@@ -1,13 +1,28 @@
 package it.unito.cassandraapiservice.model.impl.generic;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 
 public class Records extends GenericRecord{
 
-    @PrimaryKey("timestamp")
+    public Records(){}
+
+    public Records(Records records){
+        this.pezziProdotti = records.pezziProdotti;
+        this.time = records.time;
+        this.cod_odl =records.cod_odl;
+        this.codiceCommessa =records.codiceCommessa;
+        this.codiceProdotto =records.codiceProdotto;
+        this.nomeProdotto =records.nomeProdotto;
+        this.turno =records.turno;
+    }
+
+    @JsonProperty("time")
+    @PrimaryKey("time")
     private String time;
 
+    @JsonProperty("pezzi_prodotti")
     @Column("pezzi_prodotti")
     private int pezziProdotti;
 
