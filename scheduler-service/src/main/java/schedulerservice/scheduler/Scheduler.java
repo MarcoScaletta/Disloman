2c6654a4fa9e5fbfde2bd11ffbf59c67fb33c643
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-26 17:21:34 +0200

    Create first complete integration

--- scheduler-service/src/main/java/schedulerservice/scheduler/Scheduler.java
+++ scheduler-service/src/main/java/schedulerservice/scheduler/Scheduler.java
@@ -2,24 +2,26 @@ package schedulerservice.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import schedulerservice.model.records.Record;
-import schedulerservice.model.smartshareobject.odl.fasi.Monitor;
+import schedulerservice.model.Turno;
+import schedulerservice.model.cassandraobjects.OpenMonitor;
+import schedulerservice.model.records.Records;
+import schedulerservice.model.records.RecordsList;
+import schedulerservice.model.smartshareobject.*;
 import schedulerservice.requestsapi.CassandraRequests;
 import schedulerservice.requestsapi.SmartHingeRequests;
 import schedulerservice.requestsapi.SmartShareRequests;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.sql.Timestamp;
+import java.util.*;
 
 @Slf4j
 @Component
 public class Scheduler {
 
     @Autowired
-    private SmartShareRequests s;
+    private SmartShareRequests smartShareRequests;
 
     @Autowired
     private CassandraRequests cassandraRequests;
@@ -27,50 +29,296 @@ public class Scheduler {
     @Autowired
     private SmartHingeRequests smartHingeRequests;
 
-
-    @Scheduled(fixedRate = 5000, initialDelay = 5000)
     @Autowired
-    @SuppressWarnings("Duplicates")
-    public void t() throws ParseException {
-        Monitor myMonitor = new Monitor(
-                "mon_week_17_21_june",
-                new SimpleDateFormat("yyyy-MM-dd hh:mm").parse("2019-06-17 00:00").getTime() + "",
-                new SimpleDateFormat("yyyy-MM-dd hh:mm").parse("2019-06-22 00:00").getTime() + "",
-                "odl_week_17_21_june");
-        saveRecords(myMonitor);
-        System.exit(0);
+    private SchedulerLogger logger;
+
+    private final long SECONDS_ALARM_SLEEP = 30000;
+
+    @PostConstruct
+    public void task(){
+
+        logger.logFirst("FIRST SCHEDULER");
+        logger.logSecond("SECOND SCHEDULER");
+        Runnable newMonitor = () -> {
+            try {
+
+                while(true){
+                    logger.logFirst("Executing task...");
+                    if(!newMonitor()){
+                        logger.logFirst("ERROR DETECTED -> GOING TO SLEEP");
+                        Thread.sleep(SECONDS_ALARM_SLEEP/2);
+                        logger.logFirst("               " +
+                                "-> WAKE UP IN " + SECONDS_ALARM_SLEEP/2000 +" SECONDS");
+                        Thread.sleep(SECONDS_ALARM_SLEEP/2);
+
+                    }
+                    Thread.sleep(5000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable oldMonitor = () -> {
+            try {
+                while(true){
+
+                    logger.logSecond("Executing task...");
+                    if(!oldMonitor()){
+                        logger.logSecond("ERROR DETECTED -> GOING TO SLEEP");
+                        Thread.sleep(SECONDS_ALARM_SLEEP/2);
+                        logger.logSecond("               " +
+                                "-> WAKE UP IN " + SECONDS_ALARM_SLEEP/2000 +" SECONDS");
+                        Thread.sleep(SECONDS_ALARM_SLEEP/2);
+                    }
+                    Thread.sleep(5000);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        Thread threadNewMonitor = new Thread(newMonitor);
+        threadNewMonitor.start();
+        Thread threadOldMonitor = new Thread(oldMonitor);
+        threadOldMonitor.start();
+
     }
 
-    public void saveRecords(Monitor monitor){
-        List<Record> recordsTappatrice = smartHingeRequests.getTappatriceRecord(monitor);
-        List<Record> recordsEtichettatrice= smartHingeRequests.getEtichettatriceRecord(monitor);
-        List<Record> recordsIncartonatrice = smartHingeRequests.getIncartonatriceRecord(monitor);
-        List<Record> recordsBilancia = smartHingeRequests.getBilanciaRecord(monitor);
+    public boolean oldMonitor() throws Exception {
 
-        addProdCodeSaveRecords(recordsTappatrice, monitor.getCodODL());
-        addProdCodeSaveRecords(recordsEtichettatrice, monitor.getCodODL());
-        addProdCodeSaveRecords(recordsIncartonatrice, monitor.getCodODL());
-        addProdCodeSaveRecords(recordsBilancia, monitor.getCodODL());
+        Map<String, Map<String, Fase>> odlFasiMap = new HashMap<>();
+        List<Monitor> monitors = smartShareRequests.getListaMonitor().getListaMonitor();
+        List<Monitor> filteredMonitor = filterMonitor(monitors, false);
+        for (Monitor m : filteredMonitor) {
+            if (m.getTimeStop() != null)
+                if(! saveMonitor(m, true, odlFasiMap, false)){
+                    return false;
+                }
 
-        cassandraRequests.postRecordsTappatrice(recordsTappatrice);
-        cassandraRequests.postRecordsEtichettatrice(recordsEtichettatrice);
-        cassandraRequests.postRecordsIncartonatrice(recordsIncartonatrice);
-        cassandraRequests.postRecordsBilancia(recordsBilancia);
-
-        cassandraRequests.postMonitor(monitor);
+        }
+        return true;
     }
 
-    private void addProdCodeSaveRecords(List<Record> records, String prodCode){
-        for(Record r : records)
-            r.setCodiceProdotto(prodCode);
+    public boolean newMonitor() throws Exception {
+        Monitor monitor;
+        boolean isToBeClosed;
+
+        OpenMonitor openMonitor = cassandraRequests.getOpenMonitor();
+
+        if(openMonitor == null){
+            logger.logFirst("No old open monitor");
+            monitor = getOpenMonitor();
+            if(monitor != null){
+                openMonitor = new OpenMonitor();
+                openMonitor.setCodMonitor(monitor.getCodMonitor());
+                openMonitor.setStartTime(monitor.getTimeStart());
+                cassandraRequests.postOpenMonitor(openMonitor);
+            }else
+                logger.logFirst("No new open monitor");
+        }
+        if(openMonitor != null){
+            logger.logFirst("old monitor: " +openMonitor.getCodMonitor());
+            monitor = smartShareRequests.getMonitor(openMonitor.getCodMonitor());
+            monitor.setTimeStart(openMonitor.getStartTime());
+
+            if(monitor.getTimeStop() == null){
+                logger.logFirst("INFO: Il monitor ["+ monitor.getCodMonitor()+"] e' ancora aperto.");
+                logger.logFirst("INFO: Nuovo intervallo parziale"
+                        +"\n      Inizio: "+ new Timestamp(Long.parseLong(openMonitor.getStartTime()))
+                        +"\n      Fine: "  + new Timestamp(System.currentTimeMillis()));
+                monitor.setTimeStop(System.currentTimeMillis()+"");
+                isToBeClosed = false;
+            }else{
+                logger.logFirst("INFO: Il monitor ["+ monitor.getCodMonitor()+"] e' stato chiuso.");
+                logger.logFirst("\nINFO: Nuovo intervallo parziale"
+                        +"\n      Inizio: " + new Timestamp(Long.parseLong(openMonitor.getStartTime()))
+                        +"\n      Fine: " + new Timestamp(Long.parseLong(monitor.getTimeStop())));
+                isToBeClosed = true;
+            }
+            if(!saveMonitor(monitor, isToBeClosed,null, true)) {
+                return false;
+            }else{
+                openMonitor.setStartTime(monitor.getTimeStop());
+                if(isToBeClosed){
+                    cassandraRequests.deleteOpenMonitor(monitor.getCodMonitor());
+                }else{
+                    cassandraRequests.postOpenMonitor(openMonitor);
+
+                }
+            }
+        }
+        return true;
 
     }
 
-    public List<Monitor> filterMonitor(List<Monitor> monitors){
-        List<Monitor> savedMonitor = cassandraRequests.getSavedMonitors();
-        log.info("OLD SIZE MONITOR LIST: " + monitors.size());
+    public Monitor getOpenMonitor(){
+        ListaMonitor ms = smartShareRequests.getListaMonitor();
+        for(Monitor m : ms.getListaMonitor())
+            if(m.getTimeStop() == null)
+                return m;
+        return null;
+    }
+
+    public boolean saveMonitor(Monitor m, boolean closeMonitor, Map<String, Map<String, Fase>> odlFasiMap, boolean first) throws Exception {
+        String codODL, codFase, codCommessa,codProdotto, nomeProdotto;
+        Date startDate,stopDate;
+        ProdottoLavorato prodottoLavorato;
+        Fase fase = null;
+        String turno;
+
+        if (m.getTimeStop() != null) {
+            codODL = m.getCodODL();
+            codFase = m.getCodFase();
+            startDate = new Date(Long.parseLong(m.getTimeStart()));
+            stopDate = new Date(Long.parseLong(m.getTimeStop()));
+
+            if(odlFasiMap==null){
+                fase = smartShareRequests.getSingleFase(codODL, codFase);
+            }else{
+
+                if (!odlFasiMap.containsKey(m.getCodODL()))
+                    odlFasiMap.put(m.getCodODL(), new HashMap<>());
+
+                if (!odlFasiMap.get(m.getCodODL()).containsKey(codFase)) {
+                    fase = smartShareRequests.getSingleFase(codODL, codFase);
+                    odlFasiMap.get(m.getCodODL()).put(codFase, fase);
+                } else{
+                    try {
+                        fase = odlFasiMap.get(m.getCodODL()).get(codFase);
+                    }catch(Exception e){
+                        logger.err(codFase);
+                        logger.err(odlFasiMap.toString());
+                    }
+                }
+            }
+
+            if(fase != null) {
+
+                if (fase.getListaProdottiLavorati().size() < 1) {
+                    String log = "ERR: Lista prodotti lavorati vuota.";
+                    if(first)
+                        logger.logFirst(log);
+                    else
+                        logger.logSecond(log);
+                    return false;
+                }
+
+
+                prodottoLavorato = fase.getListaProdottiLavorati().get(0);
+                codCommessa = prodottoLavorato.getCodCommessa();
+                codProdotto = prodottoLavorato.getCodProdotto();
+                nomeProdotto = prodottoLavorato.getNomeProdotto();
+                turno = Turno.getTurno(m.getTimeStart()) + "";
+                String log =("\nMonitor: " + m.getCodMonitor()
+                        +"\n>>     Start: " + startDate
+                        +"\n>>      Stop: " + stopDate
+                        + "\n>>      Fase: " + codFase
+                        +"\n>>       ODL: " + codODL
+                        +"\n>>  Commessa: " + codCommessa
+                        +"\n>>  Prodotto: " + codProdotto);
+                if(first)
+                    logger.logFirst(log);
+                else
+                    logger.logSecond(log);
+
+                return saveRecords(m,
+                        codProdotto,
+                        nomeProdotto,
+                        codCommessa,
+                        codODL,
+                        turno,
+                        closeMonitor);
+            }
+        }else
+            throw new Exception("Stop time of monitor is NULL");
+        return true;
+    }
+
+
+
+    public boolean saveRecords(Monitor monitor,
+                               String codiceProdotto,
+                               String nomeProdotto,
+                               String codiceCommessa,
+                               String codiceODL,
+                               String turno,
+                               boolean closeMonitor){
+        Date start,stop;
+        if(monitor.getTimeStop() != null){
+            start = new Date(Long.parseLong(monitor.getTimeStart()));
+            stop = new Date(Long.parseLong(monitor.getTimeStop()));
+
+            RecordsList recordsTappatrice = new RecordsList();
+            RecordsList recordsEtichettatrice= new RecordsList();
+            RecordsList recordsIncartonatrice = new RecordsList();
+            RecordsList recordsBilancia = new RecordsList();
+
+            Records genericRecords = new Records();
+
+            genericRecords.setCodiceProdotto(codiceProdotto);
+            genericRecords.setNomeProdotto(nomeProdotto);
+            genericRecords.setCodiceCommessa(codiceCommessa);
+            genericRecords.setCodiceODL(codiceODL);
+            genericRecords.setTurno(turno);
+
+            try {
+                recordsTappatrice.getRecordsList().addAll(smartHingeRequests.getTappatriceRecord(start, stop));
+                recordsEtichettatrice.getRecordsList().addAll(smartHingeRequests.getEtichettatriceRecord(start, stop));
+                recordsIncartonatrice.getRecordsList().addAll(smartHingeRequests.getIncartonatriceRecord(start, stop));
+                recordsBilancia.getRecordsList().addAll(smartHingeRequests.getBilanciaRecord(start, stop));
+
+                setCommonProperties(recordsTappatrice, genericRecords);
+                setCommonProperties(recordsEtichettatrice, genericRecords);
+                setCommonProperties(recordsIncartonatrice, genericRecords);
+                setCommonProperties(recordsBilancia, genericRecords);
+
+                cassandraRequests.postRecordsTappatrice(recordsTappatrice);
+                cassandraRequests.postRecordsEtichettatrice(recordsEtichettatrice);
+                cassandraRequests.postRecordsIncartonatrice(recordsIncartonatrice);
+                cassandraRequests.postRecordsBilancia(recordsBilancia);
+            }catch (Exception e){
+                return false;
+            }
+            if(closeMonitor)
+                cassandraRequests.postMonitor(monitor);
+
+            return true;
+
+        }
+        return false;
+
+    }
+
+    private void setCommonProperties(RecordsList records, Records genericRecords){
+        for(Records r : records.getRecordsList())
+            setCommonProperties(r, genericRecords);
+
+    }
+
+    private void setCommonProperties(Records toSet, Records toCopy){
+        toSet.setCodiceProdotto(toCopy.getCodiceProdotto());
+        toSet.setNomeProdotto(toCopy.getNomeProdotto());
+        toSet.setCodiceCommessa(toCopy.getCodiceCommessa());
+        toSet.setCodiceODL(toCopy.getCodiceODL());
+        toSet.setTurno(toCopy.getTurno());
+
+    }
+
+    public List<Monitor> filterMonitor(List<Monitor> monitors, boolean first){
+        List<Monitor> savedMonitor = cassandraRequests.getClosedMonitors();
+        OpenMonitor openMonitors = cassandraRequests.getOpenMonitor();
+        if(openMonitors != null){
+            Monitor monitor = new Monitor();
+            monitor.setCodMonitor(openMonitors.getCodMonitor());
+            monitors.remove(monitor);
+        }
         monitors.removeAll(savedMonitor);
-        log.info("NEW SIZE MONITOR LIST: " + monitors.size());
+
+        String log = ("Number of missed monitor: " + monitors.size());
+        if(first)
+            logger.logFirst(log);
+        else
+            logger.logSecond(log);
         return monitors;
     }
 
