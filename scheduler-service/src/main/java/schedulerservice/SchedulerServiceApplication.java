41308a96a058f0c070b7b408fc1763b1242c4aa1
Marco Scaletta <scalettamarco@yahoo.it>
2019-07-26 17:22:32 +0200

    Read token from env var + clean code

--- scheduler-service/src/main/java/schedulerservice/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/schedulerservice/SchedulerServiceApplication.java
@@ -1,45 +1,65 @@
 package schedulerservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
 import schedulerservice.requestsapi.CassandraRequests;
 import schedulerservice.requestsapi.SmartShareRequests;
 
 import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.sql.Timestamp;
+import java.util.Scanner;
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = "schedulerservice")
 @Slf4j@Component
+@PropertySource("classpath:properties.yml")
 public class SchedulerServiceApplication {
+
+
+	@Value(value = "${orchestra.token}")
+	private String token;
 
 	@Bean
 	@Scope(value = "singleton")
-	public HttpEntity<String> httpEntitySmartShare(){
+	public HttpEntity<String> httpEntitySmartShare() throws FileNotFoundException {
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("Access-token",
-				"");
+		headers.set("Access-token",token);
 		return new HttpEntity<>(null, headers);
 	}
 
 	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	@Bean
 	public RestTemplate restTemplate(){
+
 		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
 		SSLContext sslContext = null;
@@ -47,11 +67,7 @@ public class SchedulerServiceApplication {
 			sslContext = org.apache.http.ssl.SSLContexts.custom()
 					.loadTrustMaterial(null, acceptingTrustStrategy)
 					.build();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (KeyManagementException e) {
-			e.printStackTrace();
-		} catch (KeyStoreException e) {
+		} catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
 			e.printStackTrace();
 		}
 
@@ -66,52 +82,27 @@ public class SchedulerServiceApplication {
 
 		requestFactory.setHttpClient(httpClient);
 
-		RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-		return restTemplate;
+		return new RestTemplate(requestFactory);
 	}
 
 	@Bean
 	@Scope(value = "singleton")
 	public String smartshareAPIAddress(){
-		return "https://localhost:9000";
+		return "https://192.168.120.30:9000";
 	}
 
 	@Bean
 	@Scope(value = "singleton")
 	public String smartHingeAPIServiceAddress(){
-		return "http://smarthinge-api:8081";
+		return "http://smarthinge-api:8090";
 	}
 
 	@Bean
 	@Scope(value = "singleton")
 	public String cassandraAPIServiceAddress(){
-		return "http://cassandra-api:8080";
+		return "http://cassandra-api.dl.ssbprogetti.it";
 	}
-
-    @Bean
-    @Scope(value = "singleton")
-    public String tappatriceRecordsTableName(){
-        return "records_tappatrice";
-    }
-
-	@Bean
-	@Scope(value = "singleton")
-	public String incartonatriceRecordsTableName(){
-		return "records_incartonatrice";
-	}
-
-    @Bean
-    @Scope(value = "singleton")
-    public String bilanciaRecordsTableName(){
-        return "records_bilancia";
-    }
-
-    @Bean
-    @Scope(value = "singleton")
-    public String etichettatriceRecordsTableName(){
-        return "records_etichettatrice";
-    }
 
 	@Bean
 	@Scope(value = "singleton")
@@ -121,7 +112,7 @@ public class SchedulerServiceApplication {
 
 	@Bean
 	@Scope(value = "singleton")
-    public CassandraRequests cassandraRequests() {
+	public CassandraRequests cassandraRequests() {
 		return new CassandraRequests();
 	}
 
